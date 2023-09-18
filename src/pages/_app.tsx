0a31ced583d9cbcca74ff29b7232ebd17f83cf17
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2023-09-17 23:53:43 -0300

    feat: Created blog page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
+import Head from "next/head";
+import type { AppProps } from "next/app";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Blog</title>
+        <meta
+          name="description"
+          content="A full-stack NextJS course by Coding in Flow"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={inter.className}>
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </>
+  );
 }
---
+++ src/pages/blog.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function BlogPage() {
+  return (
+    <>
+      <Head>
+        <title>Articles - Blog</title>
+        <meta name="description" content="Read the coolest blog posts!" />
+      </Head>
+      <div>blog</div>
+    </>
+  );
+}
