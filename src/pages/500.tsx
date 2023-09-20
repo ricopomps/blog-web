7db76f7e79818dfa7d92e26dd89d8dcaa1dd41a8
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2023-09-19 23:03:38 -0300

    Feat: Handling errors

---
+++ src/pages/500.tsx
@@ -0,0 +1,11 @@
+export default function ErrorPage() {
+  return (
+    <div>
+      <h1>Error</h1>
+      <p>
+        Looks like something went wrong. Please refresh the page or contact
+        support
+      </p>
+    </div>
+  );
+}
