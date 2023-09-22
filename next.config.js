5b133957ab1e4ae980d651ef324b4a3dfe7d29ec
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2023-09-22 13:04:20 -0300

    Change next config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost"],
+    domains: ["localhost", "hpm58d6xookcs2il.public.blob.vercel-storage.com"],
     deviceSizes: [576, 768, 992, 1200, 1400], //Bootsraps sizes
   },
 };
